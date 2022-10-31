0b65ca24d8c575f0b5fe9e9efecf7fe3ccb7f3de
tsinghualei <984770193@qq.com>
2022-10-31 17:35:33 +0800

    [feat]添加nacos的服务发现

---
+++ Nacos-Demo-9001/src/test/java/com/example/nacosdemo9001/NacosDemo9001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacosdemo9001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDemo9001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
